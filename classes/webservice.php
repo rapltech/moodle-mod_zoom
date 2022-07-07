63d95b47f744edfd6d7cd18e497e1a7ba40a4501
Ashish Srivastav <ashish@getrapl.com>
2022-07-07 13:10:48 +0530

    Mention algo argument in encode()

--- classes/webservice.php
+++ classes/webservice.php
@@ -128,7 +128,7 @@ class mod_zoom_webservice {
             'iss' => $this->apikey,
             'exp' => time() + 40
         );
-        $token = \Firebase\JWT\JWT::encode($payload, $this->apisecret);
+        $token = \Firebase\JWT\JWT::encode($payload, $this->apisecret, 'HS256');
         $curl->setHeader('Authorization: Bearer ' . $token);
         if ($method != 'get') {
             $curl->setHeader('Content-Type: application/json');
