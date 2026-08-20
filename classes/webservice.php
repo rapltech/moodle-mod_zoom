566fa44facc08d69c418f9da8157177cf5e341d8
Ashish Srivastav <ashish@getrapl.com>
2026-08-20 13:44:02 +0530

    Remove regular expression check to check for 1 or 2 / in instance id

--- classes/webservice.php
+++ classes/webservice.php
@@ -1084,11 +1084,8 @@ class mod_zoom_webservice {
      */
     public function get_meeting_recording($meeting_id)
     {
-        if (preg_match('#^/{1,2}#', $meeting_id)) {
-            $encoded_meeting_id = rawurlencode(rawurlencode($meeting_id));
-        } else {
-            $encoded_meeting_id = $meeting_id;
-        }
+        $encoded_meeting_id = rawurlencode(rawurlencode($meeting_id));
+
         return $this->make_call('meetings/'.$encoded_meeting_id.'/recordings?include_fields=download_access_token');
     }
 
